7be400444b76e9639f4c9e0293518a7750b7ab4c
shekshaa <shekshaa.75@gmail.com>
2017-12-04 14:25:19 +0330

    Making folder for Phase1

---
+++ Phase1/syscall1.c
@@ -0,0 +1,15 @@
+#include <sys/time.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(){
+	struct timeval tv;// A struct having seconds and microseconds
+	struct tm* ptm;// A struct having year since 1900, month, day of month, hour, minute, second, day of week and day of year
+	char time_string[40];
+	gettimeofday(&tv, NULL);// this function put the current time in seconds in value of first argumet's pointer and time zone in value of second argumet's pointer
+	ptm = localtime(&tv.tv_sec);// this function reform time in seconds to form of tm struct
+	strftime(time_string, sizeof(time_string), "%Y-%m-%d %H:%M:%S", ptm);// this function reform the tm struct format to string form
+	printf("%s\n", time_string);// finally prints time in stdout
+	return 0;
+}
+
